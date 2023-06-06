803688ddb09b602b4aa2bca0aa7aefdf45968ddd
Ehtesham Shahzad <ehtesham@we-over-i.com>
2023-06-06 17:39:45 +0500

    Setting up the project for graphql.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from './orm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      include: [],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ConfigModule.forRoot(
+      {
+        envFilePath: ['.env.prod', '.env.beta', '.env.dev', '.env'],
+        ignoreEnvFile: false,
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRoot(config),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/orm.config.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+export const config: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: process.env.DATABASE_HOST as string,
+    port: parseInt(process.env.DATABASE_PORT as string, 10),
+    username: process.env.DATABASE_USER as string,
+    password: process.env.DATABASE_PASSWORD as string,
+    database: process.env.DATABASE_NAME as string,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/db/migrations/*.js'],
+    autoLoadEntities: true,
+    synchronize: true,
+    ssl: {
+        rejectUnauthorized: true
+    }
+    // logging: true
+}
