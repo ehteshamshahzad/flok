1c299c7291b3a72dff213e2bb0815961d49ae94f
Ehtesham Shahzad <ehtesham@we-over-i.com>
2023-06-14 14:25:11 +0500

    Setting ids

--- src/users/seed.ts
+++ src/users/seed.ts
@@ -4,7 +4,7 @@ import { User } from "./entities/user.entity";
 
 export const seedUsers = [
     {
-        id: '',
+        id: '4a1b9056-4844-4964-8438-4c2be59e499c',
         name: 'Ehtesham Shahzad',
         email: 'ehteshamshahzad@hotmail.com',
         password: '^#^5c^IX11$3',
@@ -12,7 +12,7 @@ export const seedUsers = [
         accountStatus: AccountStatus.ACTIVE
     },
     {
-        id: '',
+        id: '41463052-0372-450d-83cc-a5a1817d7905',
         name: 'Daniyal Sultan',
         email: 'daniyalsultan8@gmail.com',
         password: 'E!!^$595H3lf',
@@ -20,7 +20,7 @@ export const seedUsers = [
         accountStatus: AccountStatus.ACTIVE
     },
     {
-        id: '',
+        id: '3ba74e71-194a-46f9-a2cd-f638a98e0ac0',
         name: 'Philip Cutting',
         email: 'cuttingphilip@gmail.com',
         password: '3&4L9%Bo3q#&',
@@ -28,7 +28,7 @@ export const seedUsers = [
         accountStatus: AccountStatus.ACTIVE
     },
     {
-        id: '',
+        id: '11d63a9f-4712-431a-9e2f-d3152f8b04ca',
         name: 'Ahtisham Badar',
         email: 'ahtisham.rajpoot.badar@gmail.com',
         password: '$*Kz2%5#4T8m',
